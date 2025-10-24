22cd549fc0c56bd5eaa9d23c601429948f2ed6bd
rayyan-innova360 <muhammadrayyan@innova360.io>
2025-10-24 21:31:54 +0500

    feat: set up routing with MainLayout and AiDevelopmentServices components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import  AiDevelopmentServices from "./pages/services/aiDevelopmentServices/index"
+import MainLayout from "./layouts/MainLayout"
 
-import './App.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    // errorElement: <ErrorPage />,
+    children: [
+      { path: "/services/aiDevelopmentServices", element: <AiDevelopmentServices />},
+    ],
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-      <div>
-        <p  className='font-heading'>Hello </p>
-        <button className='bg-orange-100' >
-          click
-        </button>
-      </div>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/services/aiDevelopmentServices/index.tsx
@@ -0,0 +1,11 @@
+
+
+const AiDevelopmentServices = () => {
+  return (
+    <div>
+      <h1>aiDevelopmentServices</h1>
+    </div>
+  )
+}
+
+export default AiDevelopmentServices;
